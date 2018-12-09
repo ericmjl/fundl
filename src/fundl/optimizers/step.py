6f6f9e9856ca5f3bace82dd7b8f8c7e76866aa70
Eric Ma <ericmjl@users.noreply.github.com>
2018-12-09 09:41:18 -0500

    Generalized adamW to adam
    Added optional weight decay parameter instead of baking it in.

--- src/fundl/optimizers/step.py
+++ src/fundl/optimizers/step.py
@@ -1,6 +1,6 @@
 import autograd.numpy as np
 
-def adamW_step(op:dict, g: np.ndarray, i, flat_params:np.ndarray):
+def adam_step(op:dict, g: np.ndarray, i, flat_params:np.ndarray, weight_decay=False):
     """
     One step of the adamW optimizer.
 
@@ -9,10 +9,16 @@ def adamW_step(op:dict, g: np.ndarray, i, flat_params:np.ndarray):
         as flat_params.
     :param i: the iteration number
     :param flat_params: Flattened parameter vector.
+    :param weight_decay: Whether to use weight decay or not. Requires a key 'wd' in
+        the optimizer parameters dictionary.
     """
     op['m'] = (1 - op['b1']) * g      + op['b1'] * op['m']  # First  moment estimate.
     op['v'] = (1 - op['b2']) * (g**2) + op['b2'] * op['v']  # Second moment estimate.
     mhat = op['m'] / (1 - op['b1']**(i + 1))    # Bias correction.
     vhat = op['v'] / (1 - op['b2']**(i + 1))
-    flat_params = flat_params - op['step_size']*mhat/(np.sqrt(vhat) + op['eps']) - op['wd'] * flat_params
+    if weight_decay:
+        wd = op.get('wd')
+        flat_params = flat_params - op['step_size']*mhat/(np.sqrt(vhat) + op['eps']) - op['wd'] * flat_params
+    else:
+        flat_params = flat_params - op['step_size']*mhat/(np.sqrt(vhat) + op['eps'])
     return flat_params
