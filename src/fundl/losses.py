0b89daee13abd0c36c07f4b8d05853f2456d835f
Eric Ma <ericmajinglong@gmail.com>
2019-01-02 16:04:39 -0500

    Proper refactor of planarflow_vae_loss

--- src/fundl/losses.py
+++ src/fundl/losses.py
@@ -1,5 +1,5 @@
 import autograd.numpy as np
-
+from fundl.layers.normalizing_flow import K_planar_flows
 
 def cross_entropy_loss(y, y_hat, mean=True):
     """
@@ -33,13 +33,16 @@ def ae_loss(flat_params, unflattener, model, x, y):
     return -np.sum(cross_entropy_loss(y, y_hat))
 
 
-def vae_loss(flat_params, unflattener, model, encoder, x, y, model_kwargs, l2=True):
+def vae_loss(flat_params, unflattener, model, encoder, x, y, kwargs):
     """
     Variational autoencoder loss.
+
+    kwargs supported:
+    :param bool l2: Whether or not to do l2 regularization.
     """
     # Make predictions
     params = unflattener(flat_params)
-    y_hat = model(params, x, **model_kwargs)
+    y_hat = model(params, x)
     z_mean, z_log_var = encoder(params, x)
 
     # CE-loss
@@ -48,14 +51,17 @@ def vae_loss(flat_params, unflattener, model, encoder, x, y, model_kwargs, l2=Tr
     kl_loss = np.sum(gaussian_kl(z_mean, z_log_var))
 
     l2_loss = 0
+    l2 = kwargs.pop('l2')
     if l2:
+        if not isinstance(l2, bool):
+            raise TypeError('l2 should be a boolean')
         # L2-loss
         l2_loss = np.dot(flat_params, flat_params)
 
     return -ce_loss + kl_loss + l2_loss
 
 
-def planarflow_vae_loss(flat_params, unflattener, model, encoder, x, y, kwargs):
+def planarflow_vae_loss(flat_params, unflattener, model, encoder, sampler, x, y, K, l2=True):
     """
     Loss function for normalizing flow VAEs.
 
@@ -65,27 +71,34 @@ def planarflow_vae_loss(flat_params, unflattener, model, encoder, x, y, kwargs):
     :param flat_params, unflattener: Parameters, flattened, and corresponding
         unflattener
     :param model: Function that specifies the model.
-    :param encoder: Function that specifies the encoder network.
-    :param K: A list of planar flow layer names.
+        Should have signature: (params, x, K)
+    :param encoder: Function that specifies the encoder portion of the model.
+        Should have signature: (params, x)
+    :param encoder: Function that specifies the sampler portion of the model.
+        Should have signature: (z_mean, z_log_var)
     :param x, y: inputs.
+    :param K: A list of planar flow layer names.
+    :param l2: Whether or not to use L2 regularization.
     """
-    K = kwargs.pop('K')
-    model_kwargs = dict()
-    model_kwargs['K'] = K
+    # Make predictions
+    params = unflattener(flat_params)
+    y_hat = model(params, x, K)
+    z_mean, z_log_var = encoder(params, x)
 
-    vaeloss = vae_loss(
-        flat_params, unflattener, model, encoder, model_kwargs, x, y, l2=True
-    )
+    # CE-loss
+    ce_loss = np.sum(cross_entropy_loss(y, y_hat))
+    # KL-loss
+    kl_loss = np.sum(gaussian_kl(z_mean, z_log_var))
 
+    # L2-loss
+    l2_loss = 0
+    if l2:
+        l2_loss = np.dot(flat_params, flat_params)
     params = unflattener(flat_params)
 
-    sampler = kwargs.pop('sampler')
-    K_planar_flows = kwargs.pop("K_planar_flows")
-    z_mean, z_log_var = encoder(params, x)
     z = sampler(z_mean, z_log_var)
     z, log_jacobians = K_planar_flows(params, z, K)
-
     # Sum of log-det-jacobians
     ldj_loss = np.sum(log_jacobians)
 
-    return vaeloss + ldj_loss
+    return -ce_loss + kl_loss + l2_loss + ldj_loss
