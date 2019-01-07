96bc3c668b25ab00eb6dd22e066646ccb88ee979
Eric Ma <ericmajinglong@gmail.com>
2019-01-07 16:40:15 -0500

    enabled automatic discovery of package

--- src/setup.py
+++ src/setup.py
@@ -1,3 +1,3 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name="fundl", version="0.1", packages=["fundl"])
+setup(name="fundl", version="0.1", packages=find_packages())
