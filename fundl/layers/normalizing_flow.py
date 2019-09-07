1a4c354a806ed05dcb3a26a18f45ad0e02d15807
Eric Ma <ericmajinglong@gmail.com>
2019-09-07 16:20:41 -0400

    update docstrings

--- fundl/layers/normalizing_flow.py
+++ fundl/layers/normalizing_flow.py
@@ -1,6 +1,6 @@
 import jax.numpy as np
 
-from ..bijectors import planar_flow, planar_flow_log_det_jacobian
+from fundl.bijectors import planar_flow, planar_flow_log_det_jacobian
 
 
 def K_planar_flows(params, z, K):
@@ -8,10 +8,13 @@ def K_planar_flows(params, z, K):
     :param params: A dictionary of parameters.
     :param z: Samples or transformed samples.
     :param K: a list of flow layer names, e.g. flow_1, flow_2, ...
+        Assumes that each layer key maps to the correct params dictionary,
+        which should have "w", "b" and "u" as keys
+        mapped to arrays of the appropriate shapes.
     """
     log_jacobians = []
-    for name in K:
-        p = params[name]
+    for layer_name in K:
+        p = params[layer_name]
         z = planar_flow(p, z)
         j = planar_flow_log_det_jacobian(p, z)
         log_jacobians.append(j)
