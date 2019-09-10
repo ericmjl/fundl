5fe08d10c56ac2e0a47a142429e3c6137e8b7542
Eric Ma <ericmajinglong@gmail.com>
2019-09-10 05:32:06 +0200

    rework rnn module with tests

--- fundl/layers/rnn.py
+++ fundl/layers/rnn.py
@@ -1,31 +1,51 @@
+"""RNN module."""
 import jax.numpy as np
 
-from ..nonlinearities import sigmoid
+from ..activations import sigmoid
+from fundl.utils import ndims
+
+# def rnn(params: dict, x: np.array):
+#     """
+#     Vanilla RNN layer.
+#     """
+#     h_t = np.ones(params[""])
 
 
-def gru(params: dict, feats: np.array):
+def gru_step(params, x: np.array, h_t: np.array):
     """
-    gated recurrent unit layer.
+    One in the GRU.
 
-    implements the equations as stated here: https://en.wikipedia.org/wiki/Gated_recurrent_unit
+    :param params: Dictionary of parameters.
+    :param x: One row from the input data.
+    :param h_t: History vector, the output from previous step.
+    """
+    # Transform x into a row vector with an explicit sample dimension.
+    if ndims(x) == 1:
+        x = x.reshape(1, -1)
+    z_t = sigmoid(
+        np.dot(x, params["W_z"]) + np.dot(x, params["U_z"]) + params["b_z"]
+    )
+    r_t = sigmoid(
+        np.dot(x, params["W_r"]) + np.dot(params["U_r"], h_t) + params["b_r"]
+    )
+    h_t = z_t * h_t + (1 - z_t) * np.tanh(
+        np.dot(x, params["W_h"])
+        + np.dot((r_t * h_t), params["U_h"])
+        + params["b_h"]
+    )
+    return h_t
+
+
+def gru(params: dict, x: np.array):
+    """
+    Gated recurrent unit layer.
+
+    Implements the equations as stated here:
+    https://en.wikipedia.org/wiki/Gated_recurrent_unit
     """
     outputs = []
     h_t = np.ones(params["W_z"].shape[1])
-    for i, feat in enumerate(feats):
-        z_t = sigmoid(
-            np.dot(feat, params["W_z"])
-            + np.dot(feat, params["U_z"])
-            + params["b_z"]
-        )
-        r_t = sigmoid(
-            np.dot(feat, params["W_r"])
-            + np.dot(params["U_r"], h_t)
-            + params["b_r"]
-        )
-        h_t = z_t * h_t + (1 - z_t) * np.tanh(
-            np.dot(feat, params["W_h"])
-            + np.dot((r_t * h_t), params["U_h"])
-            + params["b_h"]
-        )
+    for _, row in enumerate(x):
+        h_t = gru_step(params, row, h_t)
         outputs.append(h_t)
     return np.vstack(outputs)
