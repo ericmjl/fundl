700b13e6b8b9f7a3d3a6908e98a9685a32e89bc8
Eric Ma <ericmajinglong@gmail.com>
2019-09-10 05:44:45 +0200

    add docstrings

--- fundl/layers/__init__.py
+++ fundl/layers/__init__.py
@@ -1,3 +1,4 @@
+"""Simple neural network layers."""
 import jax.numpy as np
 import numpy.random as npr
 
@@ -8,15 +9,19 @@ def dense(params, x, nonlin=identity):
     """
     "dense" layers are just affine shifts + activation functions.
 
-    Affine shifts are represented by multiplication by weights and adding biases.
+    Affine shifts are represented
+    by multiplication by weights and adding biases.
 
     Assumes that params is a dictionary with 'w' and 'b' as keys.
 
-    Activation defaults to identity, but any elementwise numpy function can be applied.
+    Activation defaults to identity,
+    but any elementwise numpy function can be applied.
 
-    :param params: A dictionary of weights. Should have "w" and "b" as keywords.
+    :param params: A dictionary of weights.
+        Should have "w" and "b" as keywords.
     :param x: Input data.
-    :param activation: A callable that applies an elementwise activation function on the output array.
+    :param activation: A callable
+        that applies an elementwise activation function on the output array.
     """
     a = nonlin(np.dot(x, params["w"]) + params["b"])
     return a
@@ -39,4 +44,5 @@ def dropout(p, x):
 
 
 def batch_norm(p, x):
+    """Batch normalization layer."""
     pass
