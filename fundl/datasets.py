5f6de800ac8147725b1b8f9524d39053e787bac9
Eric Ma <ericmajinglong@gmail.com>
2019-10-10 14:16:53 -0400

    add graph generator

--- fundl/datasets.py
+++ fundl/datasets.py
@@ -1,6 +1,6 @@
 """Dataset generators."""
 import numpy as np
-
+import networkx as nx
 
 def make_simple_sequence(n_vocab, repeat=10):
     data = []
@@ -10,3 +10,27 @@ def make_simple_sequence(n_vocab, repeat=10):
                 data.append(j)
 
     return np.asarray(data, dtype=np.int32)
+
+
+def make_graph_counting_dataset(n_graphs):
+    """
+    Graph dataset generator.
+
+    Generates a n_graphs,
+    each with a number of nodes,
+    and a set of features attached to each node
+    as node attributes.
+
+    The features are stored under the key "feats";
+    the first slot consistently has the value "1" on it,
+    while the second slot has a random integer
+    drawn from a standard Normal distribution.
+    """
+    graphs = []
+    for i in range(n_graphs):
+        n_nodes = np.random.randint(3, 10)
+        G = nx.random_regular_graph(d=3, n=n_nodes)
+        for n in G.nodes():
+            G.node[n]["features"] = np.array([1, np.random.normal()])
+        graphs.append(G)
+    return graphs
