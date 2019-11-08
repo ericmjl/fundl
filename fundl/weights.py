9cbaa40839001784e0cc88fcfe17ce1dc9210596
Eric Ma <ericmajinglong@gmail.com>
2019-11-08 10:44:50 -0500

    add l2_weights to mlstm1900 params

--- fundl/weights.py
+++ fundl/weights.py
@@ -77,5 +77,11 @@ def add_mlstm1900_params(params, name, input_dim, output_dim):
     params[name]["wmh"] = normal(split(key)[0], (output_dim, output_dim))
     params[name]["wx"] = normal(split(key)[0], (input_dim, output_dim * 4))
     params[name]["wh"] = normal(split(key)[0], (output_dim, output_dim * 4))
+
+    params[name]["gmx"] = normal(split(key)[0], (output_dim,))
+    params[name]["gmh"] = normal(split(key)[0], (output_dim,))
+    params[name]["gx"] = normal(split(key)[0], (output_dim * 4,))
+    params[name]["gh"] = normal(split(key)[0], (output_dim * 4,))
+
     params[name]["b"] = normal(split(key)[0], (output_dim * 4,))
     return params
