cf84f3f6983b4be44777ad709838b5452d1406f7
Eric Ma <ericmjl@users.noreply.github.com>
2019-12-07 15:10:54 -0500

    Batch layers (#4)
    Actually, I just did a bunch of miscellaneous stuff.
    
    * Made feedforward example JIT-compatible
    
    * Added shape annotations
    
    * Added installation script
    
    * Changed logging directory

--- examples/feedforward.py
+++ examples/feedforward.py
@@ -1,12 +1,14 @@
 """Example Feedforward neural network model."""
-from jax import grad
+from jax import grad, jit
 from jax.experimental.optimizers import adam
 
 from fundl.activations import relu
 from fundl.layers import dense
-from fundl.losses import mseloss
+from fundl.losses import mseloss, _mse_loss
 from fundl.weights import add_dense_params
 from sklearn.datasets import make_regression
+
+from time import time
 
 x, y = make_regression(n_samples=1000, n_features=10, n_informative=2)
 y = y.reshape(-1, 1)
@@ -28,15 +30,25 @@ init, update, get_params = adam(step_size=0.1)
 
 # print(model(params, x))
 # print(y)
-print(mseloss(params, model, x, y))
-dloss = grad(mseloss)
+def mseloss(params, x, y):
+    y_hat = model(params, x)
+    return _mse_loss(y, y_hat)
 
+
+print(mseloss(params, x, y))
+JIT = True
+dloss = grad(mseloss)
+if JIT:
+    dloss = jit(dloss)
+
+start = time()
 state = init(params)
 for i in range(1000):
-    g = dloss(params, model, x, y)
-    l = mseloss(params, model, x, y)
+    g = dloss(params, x, y)
+    l = mseloss(params, x, y)
 
     state = update(i, g, state)
     params = get_params(state)
 
     print(i, l)
+print(f"JIT: {JIT}, {time() - start:.2f} seconds")
